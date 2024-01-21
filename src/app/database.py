8cfbd67d0c13281b89dbd09c7e3df541f24a2cf5
Thomas Fox <TFox95@aol.com>
2024-01-21 11:36:59 -0600

    Updated list of imports, defined the get_db function to handle opening and the closing of the db connection.

--- src/app/database.py
+++ src/app/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
 
 from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @as_declarative()
