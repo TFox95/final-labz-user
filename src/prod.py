aaec0c7a4852cda739499629e341c16cee7c6cd4
Thomas Fox <TFox95@aol.com>
2024-02-08 16:40:37 -0600

    updated the way the server is ran in production

--- src/prod.py
+++ src/prod.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3.9
 
-import os
-import subprocess
+import uvicorn
 
 if __name__ == "__main__":
 
-    app_file = os.getenv("FASTAPI_APP", "app.main")
-    subprocess.call(["uvicorn", f"{app_file}:app"])
+    uvicorn.run("app.main:app", host="localhost", port=4000)
