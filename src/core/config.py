d84ddfb089836a6a7405f5cd6c0e31f09b54e976
Thomas Fox <TFox95@aol.com>
2024-02-08 16:00:03 -0600

    Added a new property to the Setting class to handle the password salt key; As well as fixed the postgresDsn URI build function.

--- src/core/config.py
+++ src/core/config.py
@@ -22,6 +22,8 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     DATABASE_URI: Optional[PostgresDsn] = None
 
+    PASSWORD_SALT_KEY: str
+
     @validator("DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
@@ -31,7 +33,7 @@ class Settings(BaseSettings):
             username=values.get("POSTGRES_USER"),
             password=values.get("POSTGRES_PASSWORD"),
             host=values.get("POSTGRES_SERVER"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
+            path=f"{values.get('POSTGRES_DB') or ''}",
         )
 
     class Config:
