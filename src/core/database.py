ead72f74b2ab222252ae9f233a981d9c175137ad
Thomas Fox <TFox95@aol.com>
2024-03-22 18:07:55 -0500

    introduce Pydantic integration and enhance SQLAlchemy models
    * **Pydantic Base Schema:** Added  class, inheriting from , to enable ORM mode and provide a base for model schemas.
    * **SQLAlchemy Model Enhancements:**
        * Modified :
            * Added   method for clean model serialization
            * Added  to improve  pickling behavior, excluding sensitive fields.
    * **Configuration Streamlining:**
         * Used explicit string conversion in   for clarity.

--- src/core/database.py
+++ src/core/database.py
@@ -1,34 +1,45 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.orm import sessionmaker, Session
-from typing import ContextManager
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel as Base
 
 from core.config import settings
 
 
 class Database:
     def __init__(self):
-        self.engine = create_engine(settings.DATABASE_URI,
-                                    pool_pre_ping=True)
+        self.engine = create_engine(str(settings.DATABASE_URI),
+                                    pool_pre_ping=True, echo=False)
         self.SessionLocal = sessionmaker(autocommit=False,
                                          autoflush=False, bind=self.engine)
 
-    def get_db(self) -> ContextManager[Session]:
+    def get_db(self):
         db = self.SessionLocal()
         try:
-            yield db
+            return db
         finally:
             db.close()
 
 
+class BaseSchema(Base):
+    """
+    Base Class for all Sqlalchemy Model Schema's
+    """
+    class Config:
+        orm_mode = True
+        # from_attributes = True
+
+
 @as_declarative()
-class Base:
+class ModelBase:
     """
     Base class for all SQLAlchemy models in this application.
 
     This class provides the following features:
 
-    * Automatic table name generation based on the class name (lowercase with an 's' suffix).
+    * Automatic table name generation based
+        on the class name (lowercase with an 's' suffix).
+
     * Standardized metadata management using a shared DeclarativeMeta class.
 
     Do not instantiate this class directly; use child classes instead.
@@ -42,7 +53,20 @@ class Base:
         Returns:
             str: The table name with lowercase name and 's' suffix.
         """
-        return f"{cls.__name__.lower()}s"
+        return f"{cls.__name__.lower()}"
+
+    def dict(self, exclude_none=True):
+        return {
+            key: value
+            for key, value in self.__dict__.items()
+            if value is not None and key not in ('_sa_instance_state')
+        }
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state['_sa_instance_state']
+        # del state['password']  # Exclude password for safety
+        return state
 
 
 database = Database()
