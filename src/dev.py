1c623acc1711a60068fee8b06092876c2fcc847a
Thomas Fox <TFox95@aol.com>
2024-03-22 17:54:10 -0500

    corrected Uvicorn invocation for app loading in development setups.

--- src/dev.py
+++ src/dev.py
@@ -7,6 +7,4 @@ if __name__ == "__main__":
 
     args = ["--reload"]
     app_file = os.getenv("FASTAPI_APP", "app.main")
-    print(app_file)
-    print(args)
     subprocess.call(["uvicorn", f"{app_file}:app", *args])
