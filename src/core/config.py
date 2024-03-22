00ad3240bad76b67ed17f19914fde08010a95d56
Thomas Fox <TFox95@aol.com>
2024-03-22 18:03:20 -0500

    add JWT config, introduce custom JSON renderer
    * **Settings Updates:**
        *  Added  for JWT-based authentication.
        *  Modified  validator for clarity (optional note)
    
    * **JSON Rendering:**
       * Introduced  class to customize API responses while maintaining a consistent data structure.
    
    * **Dependencies:**
        * May introduce a dependency on a JWT library if  is used (note this if applicable)

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,9 @@
 
 from typing import Any, Dict, List, Optional, Union
-from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl, PostgresDsn, validator
+
+from pydantic import AnyHttpUrl, PostgresDsn, validator, BaseSettings
+
+from fastapi.responses import JSONResponse
 
 
 class Settings(BaseSettings):
@@ -9,7 +11,8 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+    def assemble_cors_origins(cls, v: Union[str,
+                              List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
         elif isinstance(v, (list, str)):
@@ -22,23 +25,37 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     DATABASE_URI: Optional[PostgresDsn] = None
 
-    PASSWORD_SALT_KEY: str
+    JWT_SECRET_KEY: str
 
     @validator("DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+    def assemble_db_connection(cls, v: Optional[
+                               str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
         return PostgresDsn.build(
             scheme="postgresql+psycopg2",
-            username=values.get("POSTGRES_USER"),
+            user=values.get("POSTGRES_USER"),
             password=values.get("POSTGRES_PASSWORD"),
             host=values.get("POSTGRES_SERVER"),
-            path=f"{values.get('POSTGRES_DB') or ''}",
+            path=f"/{values.get('POSTGRES_DB') or ''}",
         )
 
     class Config:
         case_sensitive = True
         env_file = ".env"
+
+
+class JsonRender(JSONResponse):
+    """
+    This Class was created to return certain content that would
+    allow content that needs to be return as an object to utilize
+    the reponse_model argument on an API Endpoint but it keeps
+    consistency of the Apps Json-scheme.
+    """
+
+    def render(self, content: any) -> bytes:
+        # Here you can modify the response content or headers as needed
+        return super().render({'data': content})
 
 
 settings = Settings()
