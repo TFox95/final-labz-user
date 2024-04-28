702b180f399518ecb089402879254d5b5bf9342d
Thomas Fox <TFox95@aol.com>
2024-04-28 15:26:15 -0500

    refactor: simplify user model and remove unused classes
    * **Data Model Simplification:**
        * Removed the , , and  classes, as they appear to be unused or planned for future implementation.
        * Streamlined the  class by focusing on core user attributes.
    * **Code Cleanup:**
        * Removed associated code and relationships that were dependent on the deleted classes.
    * **Focus on Core Functionality:**
        * Enhanced maintainability by concentrating on essential user management  features.

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,11 +1,10 @@
 from fastapi import HTTPException, status
-from sqlalchemy import (ForeignKey, Float,
+from sqlalchemy import (ForeignKey,
                         Column, Integer,
                         String, Enum)
 from sqlalchemy.orm import relationship, joinedload
 from sqlalchemy.sql import func
 from sqlalchemy.types import DateTime
-from sqlalchemy.exc import IntegrityError, DBAPIError
 from pydantic import EmailStr
 
 from argon2 import PasswordHasher
@@ -13,7 +12,7 @@ from argon2.exceptions import (VerifyMismatchError,
                                InvalidHashError, VerificationError)
 from os import urandom
 
-from auth.enums import USER_ROLES, JOB_STATUS_STATES, CATEGORY_STATES
+from auth.enums import USER_ROLES
 from core.database import ModelBase as Base, Database
 
 
@@ -324,99 +323,3 @@ class Client_Additional(Additional):
     __mapper_args__ = {
         'polymorphic_identity': USER_ROLES.CLIENT
     }
-
-
-class Jobs(Base):
-    id = Column(Integer, primary_key=True, index=True, nullable=False)
-    title = Column(String(length=256), index=True, nullable=False)
-    description = Column(String(length=512), nullable=False)
-    category = Column(Enum(CATEGORY_STATES), nullable=False)
-    poster = relationship("Client_Additional", back_populates="posted_jobs")
-    poster_id = Column(Integer, ForeignKey(Client_Additional.id))
-    taken_by_user_id = Column(Integer, ForeignKey(Contractor_Additional.id))
-    taken_by_user = relationship("Contractor_Additional",
-                                 back_populates="jobs")
-    amount = Column(Float, nullable=False)
-    status = Column(Enum(JOB_STATUS_STATES), nullable=False,
-                    default=JOB_STATUS_STATES.UNASSIGNED.value)
-    # messages = relationship("Message", back_populates="job",
-    #                        lazy="joined", uselist=True)
-
-    def __init__(self, title: str, description: str, category: CATEGORY_STATES,
-                 amount: float, status: JOB_STATUS_STATES, poster: User):
-        self.title = title
-        self.description = description
-        self.category = category
-        self.amount = amount
-        self.poster = poster
-        self.status = status
-
-    def create(self, db: Database) -> int:
-        with db.get_db() as session:
-            session.add(self)
-            session.commit()
-            session.refresh(self)
-            return self.id
-
-    def update(self, db: Database, fields: dict):
-        with db.get_db() as session:
-            for field, value in fields.items():
-                setattr(self, field, value)
-            session.merge(self)
-            session.commit()
-            session.refresh(self)
-            return self
-
-    def get_by_id(self, db: Database, job_id: int):
-        with db.get_db() as session:
-            stored_obj: self = session.query(
-                Jobs).filter_by(id=job_id).scalar()
-            return stored_obj
-
-    def assign_contractor(self, db: Database, contractor) -> bool or str:
-        """Assigns a contractor to this job and updates the database.
-
-        Args:
-            contractor: The User object representing the contractor.
-
-        Returns:
-            True on success, a descriptive error string on failure.
-        """
-
-        try:
-            with db.get_db() as session:
-                self.takenByUser = contractor
-                session.merge(self)
-                session.commit()
-                return True
-        except IntegrityError as e:
-            # Handle potential constraint violations, foreign key errors, etc.
-            session.rollback()
-            return f"Error assigning contractor: {str(e)}, Integrity constraint violated."
-        except (DBAPIError, Exception) as e:
-            # Handle other database or unexpected errors
-            session.rollback()
-            return f"Error assigning contractor: {str(e)}"
-
-
-# class Transaction(Base):
-#    id = Column(Integer, primary_key=True, index=True, nullable=False)
-#    #amount = Column(String, ForeignKey(Jobs.amount))
-#    job_id = Column(Integer, ForeignKey(Jobs.id))
-#    job = relationship(Jobs, backref="transactions")
-#    contractor = relationship(Contractor_Additional, backref="transactions")
-#    contractor_id = relationship(Integer,
-#                                 ForeignKey(Contractor_Additional.id))
-#    transactionDate = Column(DateTime(timezone=True),
-#                             server_default=func.now(), nullable=True)
-
-
-# class Message(Base):
-#    id = Column(Integer, primary_key=True, index=True, nullable=False)
-#    title = Column(String(length=128), nullable=False)
-#    content = Column(String(length=256), nullable=False)
-#    sender = Column(Integer, ForeignKey(User.id), nullable=False)
-#    recipient = Column(Integer, ForeignKey(User.id), nullable=False)
-#    job = relationship(Jobs, back_populates="messages",
-#                       lazy="select", uselist=False)
-#    job_id = Column(Integer, ForeignKey(Jobs.id, ondelete="CASCADE"))
