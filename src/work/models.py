0282a20fd28cbbca50ab47a67482a6031736af77
Thomas Fox <TFox95@aol.com>
2024-04-28 15:37:46 -0500

    feat: add Jobs model and relationships
    * **Jobs Model:**
        * Introduced the  model to represent job postings.
        * Defined columns for , , , , , and timestamps.
    * **Relationships:**
        * Established foreign key relationships with  (poster) and  (taken_by_user).
        * Used backrefs for convenient navigation.
    * **Data Management:**
        * Implemented , , , and   methods for basic data operations.
    * **Contractor Assignment:**
        * Added the  method for managing job assignments.

--- src/work/models.py
+++ src/work/models.py
@@ -1,60 +1,119 @@
 from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy import ForeignKey, Enum, event
-from sqlalchemy.orm import relationship, Session
+from sqlalchemy import ForeignKey, Enum
+from sqlalchemy.orm import relationship
 from sqlalchemy.types import DateTime
 from sqlalchemy.sql import func
-from sqlalchemy.exc import IntegrityError, DBAPIError
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError, DBAPIError
 
-from enum import Enum as PyEnum
+from typing import Optional
 
+from auth.enums import CATEGORY_STATES, JOB_STATUS_STATES
+from auth.models import User, Client_Additional, Contractor_Additional
 from core.database import ModelBase as Base, Database
 
 
-class CATEGORY_STATES(PyEnum):
-    PC_CUSTOM_BUILD = 0
-    SOFTWARE_INSTALL = 1
-    SECURITY_HARDWARE_INSTALL = 2
-    SERVER_SETUP = 3
-    OTHER = 4
+class Jobs(Base):
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String(length=256), index=True, nullable=False)
+    description = Column(String(length=512), nullable=False)
+    category = Column(Enum(CATEGORY_STATES), nullable=False)
+    poster = relationship("Client_Additional", back_populates="posted_jobs")
+    poster_id = Column(Integer, ForeignKey(Client_Additional.id))
+    taken_by_user_id = Column(Integer, ForeignKey(Contractor_Additional.id))
+    taken_by_user = relationship("Contractor_Additional",
+                                 back_populates="jobs")
+    amount = Column(Float, nullable=False)
+    status = Column(Enum(JOB_STATUS_STATES), nullable=False,
+                    default=JOB_STATUS_STATES.UNASSIGNED.value)
+    # messages = relationship("Message", back_populates="job",
+    #                        lazy="joined", uselist=True)
+
+    def __init__(self, title: str, description: str, category: CATEGORY_STATES,
+                 amount: float, status: JOB_STATUS_STATES, poster: User):
+        self.title = title
+        self.description = description
+        self.category = category
+        self.amount = amount
+        self.poster = poster
+        self.status = status
+
+    def create(self, db: Database) -> int:
+        with db.get_db() as session:
+            session.add(self)
+            session.commit()
+            session.refresh(self)
+            return self.id
+
+    def update(self, db: Database, fields: dict) -> Optional[str]:
+        with db.get_db() as session:
+
+            for field, value in fields.items():
+                setattr(self, field, value) if (
+                    field != "category"
+                ) else setattr(self, field, CATEGORY_STATES(value).name)
+            try:
+                session.merge(self)
+                session.commit()
+            except SQLAlchemyError as exc:
+                print(str(exc))
+                return None
+        return "Job was successfully updated."
+
+    def get_by_id(db: Database, job_id: int):
+        with db.get_db() as session:
+            stored_obj: Jobs = session.query(
+                Jobs).filter_by(id=job_id).scalar()
+            return stored_obj
+
+    def get_jobs_by_userId(db: Database, user_additional_id: int):
+        with db.get_db() as session:
+            return session.query(Jobs).filter_by(
+                poster_id=user_additional_id).all()
+
+    def assign_contractor(self, db: Database, contractor) -> bool or str:
+        """Assigns a contractor to this job and updates the database.
+
+        Args:
+            contractor: The User object representing the contractor.
+
+        Returns:
+            True on success, a descriptive error string on failure.
+        """
+
+        try:
+            with db.get_db() as session:
+                self.takenByUser = contractor
+                session.merge(self)
+                session.commit()
+                return True
+        except IntegrityError as e:
+            # Handle potential constraint violations, foreign key errors, etc.
+            session.rollback()
+            return f"Error assigning contractor: {str(e)}, Integrity constraint violated."
+        except (DBAPIError, Exception) as e:
+            # Handle other database or unexpected errors
+            session.rollback()
+            return f"Error assigning contractor: {str(e)}"
 
 
-class JOB_STATUS_STATES(PyEnum):
-    UNASSIGNED = 0
-    ASSIGNED = 1
-    IN_PROGRESS = 2
-    PENDING = 3
-    COMPLETED = 4
-    CANCELLED = 5
-
-
-
-
-#class Transaction(Base):
+# class Transaction(Base):
 #    id = Column(Integer, primary_key=True, index=True, nullable=False)
-#    amount = Column(String, ForeignKey(Jobs.amount))
-#    jobId = Column(Integer, ForeignKey(Jobs.id))
-#    job = relationship("Job", backref="transactions")
-#    contractor = relationship("Contractor", backref="transactions")
+#    #amount = Column(String, ForeignKey(Jobs.amount))
+#    job_id = Column(Integer, ForeignKey(Jobs.id))
+#    job = relationship(Jobs, backref="transactions")
+#    contractor = relationship(Contractor_Additional, backref="transactions")
+#    contractor_id = relationship(Integer,
+#                                 ForeignKey(Contractor_Additional.id))
 #    transactionDate = Column(DateTime(timezone=True),
 #                             server_default=func.now(), nullable=True)
 
 
-#class Message(Base):
+# class Message(Base):
 #    id = Column(Integer, primary_key=True, index=True, nullable=False)
 #    title = Column(String(length=128), nullable=False)
 #    content = Column(String(length=256), nullable=False)
-#    sender = Column(Integer, ForeignKey(UserBaseModel.id), nullable=False)
-#    recipient = Column(Integer, ForeignKey(UserBaseModel.id), nullable=False)
-#   postedOnJob = relationship("Job", back_populates="messages", )
-
-
-#@event.listens_for(Job, "after_update")
-#def update_contractor_jobs(mapper, connection, target: Job):
-#    with Database().get_db() as db:  # Acquire session within the listener
-#        db: Session = db
-#        if target.status == JOB_STATUS_STATES.COMPLETED and target.takenByUser:
-#            contractor = target.takenByUser
-#            contractor.activeJobs.remove(target)
-#            contractor.completedJobs.append(target)
-#            db.add(contractor)  # Update contractor in database
-#            db.commit()
+#    sender = Column(Integer, ForeignKey(User.id), nullable=False)
+#    recipient = Column(Integer, ForeignKey(User.id), nullable=False)
+#    job = relationship(Jobs, back_populates="messages",
+#                       lazy="select", uselist=False)
+#    job_id = Column(Integer, ForeignKey(Jobs.id, ondelete="CASCADE"))
